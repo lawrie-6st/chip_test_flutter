b692f055c27dc5af3eab3d29ccdc5c0188146725
Yajima Wataru <wataru.yajima@globis.co.jp>
2023-09-28 10:54:36 +0900

    use textfield_tags

--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,254 @@
 import 'package:flutter/material.dart';
+import 'package:textfield_tags/textfield_tags.dart';
 
-void main() => runApp(const MyApp());
+// https://github.com/eyoeldefare/textfield_tags/blob/master/example/lib/main.dart
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Woolha.com Flutter Tutorial',
-      home: _InputChipExample(),
+      title: 'Flutter Demo',
+      theme: ThemeData(primarySwatch: Colors.green),
+      home: Home(),
     );
   }
 }
 
-class _InputChipExample extends StatefulWidget {
+class Home extends StatefulWidget {
   @override
-  _InputChipExampleState createState() => _InputChipExampleState();
+  State<Home> createState() => _HomeState();
 }
 
-class _InputChipExampleState extends State<_InputChipExample> {
-  final TextEditingController _textEditingController = TextEditingController();
-  List<String> _values = [];
-  final List<String> _suggestion = [
-    'guitar',
-    'read book',
-    'watch movie',
-    'play game'
-  ];
+class _HomeState extends State<Home> {
+  late double _distanceToField;
+  late TextfieldTagsController _controller;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    _distanceToField = MediaQuery.of(context).size.width;
+  }
+
   @override
   void dispose() {
-    _textEditingController.dispose();
     super.dispose();
-  }
-
-  Widget buildChips() {
-    List<Widget> chips = [];
-
-    for (int i = 0; i < _values.length; i++) {
-      InputChip actionChip = InputChip(
-        selected: false,
-        label: Text(_values[i]),
-        backgroundColor: Colors.blue[100],
-        onDeleted: () {
-          _values.removeAt(i);
-          setState(() {
-            _values = _values;
-          });
-        },
-      );
-
-      chips.add(actionChip);
-    }
-
-    return ListView(
-      // This next line does the trick.
-      scrollDirection: Axis.horizontal,
-      children: chips,
-    );
+    _controller.dispose();
   }
 
   @override
+  void initState() {
+    super.initState();
+    _controller = TextfieldTagsController();
+  }
+
+  static const List<String> _pickLanguage = <String>[
+    'c',
+    'c++',
+    'java',
+    'python',
+    'javascript',
+    'php',
+    'sql',
+    'yaml',
+    'gradle',
+    'xml',
+    'html',
+    'flutter',
+    'css',
+    'dockerfile'
+  ];
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Chip & Autocomplete'),
+    return MaterialApp(
+      title: "wellcome",
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: const Color.fromARGB(255, 74, 137, 92),
+          centerTitle: true,
+          title: const Text('Enter a tag...'),
+        ),
+        body: Column(
+          children: [
+            Autocomplete<String>(
+              optionsViewBuilder: (context, onSelected, options) {
+                return Container(
+                  margin: const EdgeInsets.symmetric(
+                      horizontal: 10.0, vertical: 4.0),
+                  child: Align(
+                    alignment: Alignment.topCenter,
+                    child: Material(
+                      elevation: 4.0,
+                      child: ConstrainedBox(
+                        constraints: const BoxConstraints(maxHeight: 200),
+                        child: ListView.builder(
+                          shrinkWrap: true,
+                          itemCount: options.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            final dynamic option = options.elementAt(index);
+                            return TextButton(
+                              onPressed: () {
+                                onSelected(option);
+                              },
+                              child: Align(
+                                alignment: Alignment.centerLeft,
+                                child: Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      vertical: 15.0),
+                                  child: Text(
+                                    '#$option',
+                                    textAlign: TextAlign.left,
+                                    style: const TextStyle(
+                                      color: Color.fromARGB(255, 74, 137, 92),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                    ),
+                  ),
+                );
+              },
+              optionsBuilder: (TextEditingValue textEditingValue) {
+                if (textEditingValue.text == '') {
+                  return const Iterable<String>.empty();
+                }
+                return _pickLanguage.where((String option) {
+                  return option.contains(textEditingValue.text.toLowerCase());
+                });
+              },
+              onSelected: (String selectedTag) {
+                _controller.addTag = selectedTag;
+              },
+              fieldViewBuilder: (context, ttec, tfn, onFieldSubmitted) {
+                return TextFieldTags(
+                  textEditingController: ttec,
+                  focusNode: tfn,
+                  textfieldTagsController: _controller,
+                  initialTags: const [
+                    'pick',
+                    'your',
+                    'favorite',
+                    'programming',
+                    'language',
+                  ],
+                  textSeparators: const [' ', ','],
+                  letterCase: LetterCase.normal,
+                  validator: (String tag) {
+                    if (tag == 'php') {
+                      return 'No, please just no';
+                    } else if (_controller.getTags!.contains(tag)) {
+                      return 'you already entered that';
+                    }
+                    return null;
+                  },
+                  inputfieldBuilder:
+                      (context, tec, fn, error, onChanged, onSubmitted) {
+                    return ((context, sc, tags, onTagDelete) {
+                      return Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                        child: TextField(
+                          controller: tec,
+                          focusNode: fn,
+                          decoration: InputDecoration(
+                            border: const UnderlineInputBorder(
+                              borderSide: BorderSide(
+                                  color: Color.fromARGB(255, 74, 137, 92),
+                                  width: 3.0),
+                            ),
+                            focusedBorder: const UnderlineInputBorder(
+                              borderSide: BorderSide(
+                                  color: Color.fromARGB(255, 74, 137, 92),
+                                  width: 3.0),
+                            ),
+                            helperText: 'Enter language...',
+                            helperStyle: const TextStyle(
+                              color: Color.fromARGB(255, 74, 137, 92),
+                            ),
+                            hintText: _controller.hasTags ? '' : "Enter tag...",
+                            errorText: error,
+                            prefixIconConstraints: BoxConstraints(
+                                maxWidth: _distanceToField * 0.74),
+                            prefixIcon: tags.isNotEmpty
+                                ? SingleChildScrollView(
+                                    controller: sc,
+                                    scrollDirection: Axis.horizontal,
+                                    child: Row(
+                                        children: tags.map((String tag) {
+                                      return Container(
+                                        decoration: const BoxDecoration(
+                                          borderRadius: BorderRadius.all(
+                                            Radius.circular(20.0),
+                                          ),
+                                          color:
+                                              Color.fromARGB(255, 74, 137, 92),
+                                        ),
+                                        margin:
+                                            const EdgeInsets.only(right: 10.0),
+                                        padding: const EdgeInsets.symmetric(
+                                            horizontal: 10.0, vertical: 4.0),
+                                        child: Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.spaceBetween,
+                                          children: [
+                                            InkWell(
+                                              child: Text(
+                                                '#$tag',
+                                                style: const TextStyle(
+                                                    color: Colors.white),
+                                              ),
+                                              onTap: () {
+                                                //print("$tag selected");
+                                              },
+                                            ),
+                                            const SizedBox(width: 4.0),
+                                            InkWell(
+                                              child: const Icon(
+                                                Icons.cancel,
+                                                size: 14.0,
+                                                color: Color.fromARGB(
+                                                    255, 233, 233, 233),
+                                              ),
+                                              onTap: () {
+                                                onTagDelete(tag);
+                                              },
+                                            )
+                                          ],
+                                        ),
+                                      );
+                                    }).toList()),
+                                  )
+                                : null,
+                          ),
+                          onChanged: onChanged,
+                          onSubmitted: onSubmitted,
+                        ),
+                      );
+                    });
+                  },
+                );
+              },
+            ),
+            ElevatedButton(
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(
+                  const Color.fromARGB(255, 74, 137, 92),
+                ),
+              ),
+              onPressed: () {
+                _controller.clearTags();
+              },
+              child: const Text('CLEAR TAGS'),
+            ),
+          ],
+        ),
       ),
-      body: Padding(
-          padding: const EdgeInsets.all(15.0),
-          child: Column(
-            children: <Widget>[
-              SizedBox(
-                height: 30,
-                child: buildChips(),
-              ),
-              Autocomplete(
-                optionsBuilder: (TextEditingValue textEditingValue) {
-                  if (textEditingValue.text == '') {
-                    return const Iterable<String>.empty();
-                  }
-                  return _suggestion.where((String option) {
-                    return option.contains(textEditingValue.text.toLowerCase());
-                  });
-                },
-                onSelected: (String selection) {
-                  _values.add(selection);
-                  _textEditingController.clear();
-
-                  setState(() {
-                    _values = _values;
-                  });
-                },
-                fieldViewBuilder: (BuildContext context,
-                    TextEditingController textEditingController,
-                    FocusNode focusNode,
-                    VoidCallback onFieldSubmitted) {
-                  return TextFormField(
-                    controller: textEditingController,
-                    focusNode: focusNode,
-                    onFieldSubmitted: (String value) {
-                      _values.add(value);
-                      textEditingController.clear();
-
-                      setState(() {
-                        _values = _values;
-                        focusNode.requestFocus();
-                      });
-                    },
-                  );
-                },
-              ),
-            ],
-          )),
     );
   }
 }
